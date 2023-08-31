45ae22a3107487297f38b4983b9bb25bc216f178
sipovalovdmitro <sipovalovdmitro@gmail.com>
2023-08-31 12:45:25 +0300

    init

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :tasks
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "tasks#index"
+  post "tasks/:id/toggle", to: "tasks#toggle"
+end
